9532bb70aac3f8cc7b0a45d4c07fa5071764c1db
Steven Leiva <leiva.steven@gmail.com>
2015-10-22 17:29:29 -0400

    Add User model.

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  has_many :questions
+  has_many :answers
+  has_many :comments
+  has_many :votes
+
+  validates :username, presence: true, uniqueness: true
+  validates :password, confirmation: true
+end

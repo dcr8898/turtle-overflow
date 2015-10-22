7dfa0503e19ee9ad2f973d55f37a0fe3d31fd59e
Steven Leiva <leiva.steven@gmail.com>
2015-10-22 18:40:16 -0400

    Add uniqueness constraint on the votes table - (unique on combination of user_id, voteable_id, and voteable_type).

---
+++ db/migrate/20151022223440_add_uniqueness_contraint_to_vote.rb
@@ -0,0 +1,5 @@
+class AddUniquenessContraintToVote < ActiveRecord::Migration
+  def change
+    add_index :votes, [:user_id, :voteable_id, :voteable_type], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151022211350) do
+ActiveRecord::Schema.define(version: 20151022223440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,6 +101,7 @@ ActiveRecord::Schema.define(version: 20151022211350) do
 
   add_index "votes", ["created_at"], name: "index_votes_on_created_at", using: :btree
   add_index "votes", ["updated_at"], name: "index_votes_on_updated_at", using: :btree
+  add_index "votes", ["user_id", "voteable_id", "voteable_type"], name: "index_votes_on_user_id_and_voteable_id_and_voteable_type", unique: true, using: :btree
   add_index "votes", ["user_id"], name: "index_votes_on_user_id", using: :btree
   add_index "votes", ["voteable_type", "voteable_id"], name: "index_votes_on_voteable_type_and_voteable_id", using: :btree
 

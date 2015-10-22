099ee28678b3287fbd6e75382c6e2fc7d3260f81
Steven Leiva <leiva.steven@gmail.com>
2015-10-22 17:00:45 -0400

    Add questions migration.

---
+++ db/migrate/20151022205641_create_questions.rb
@@ -0,0 +1,12 @@
+class CreateQuestions < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+      t.string :title, null: false
+      t.string :body, null: false
+      t.references :user, null: false, index: true
+      t.references :answer, index: true
+
+      t.timestamps null: false, index: true
+    end
+  end
+end

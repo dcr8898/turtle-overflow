2d768e5de10ed72d48c23c7ef1e90f7d3f3cfbdf
Steven Leiva <leiva.steven@gmail.com>
2015-10-22 17:02:29 -0400

    Change body from string to text on answers and questions migration.

--- db/migrate/20151022205021_create_answers.rb
+++ db/migrate/20151022205021_create_answers.rb
@@ -1,7 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-      t.string :body, null: false
+      t.text :body, null: false
       t.references :user, null: false, index: true
       t.references :question, null: false, index: true
 
--- db/migrate/20151022205641_create_questions.rb
+++ db/migrate/20151022205641_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
       t.string :title, null: false
-      t.string :body, null: false
+      t.text :body, null: false
       t.references :user, null: false, index: true
       t.references :answer, index: true
 

e2cfa5f71c403bf27a175b4e82845beb2af1bfb4
Steven Leiva <leiva.steven@gmail.com>
2015-10-22 16:57:30 -0400

    Add indexes to users table.

--- db/migrate/20151022204752_create_users.rb
+++ db/migrate/20151022204752_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username, null: false
+      t.string :username, null: false, index: true
       t.string :email
       t.string :password_digest, null: false
 
-      t.timestamps null: false
+      t.timestamps null: false, index: true
     end
   end
 end

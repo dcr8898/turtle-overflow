78cb6b6e9cf18bfe791d106b1f62fea71a09985d
Damian C. Rossney <dcrossney@gmail.com>
2015-10-23 16:00:02 -0400

    Bugfix for seed file
    Corrects various bugs preventing seed generation.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # Users
-print "Creatings users . . . "
+print "Seeding users . . . "
 users = Array.new(50) { Faker::Internet.user_name }.uniq
 25.times do |i|
     User.create(username: users[i],
@@ -9,7 +9,7 @@ puts "Done!"
 user_ids = User.ids
 
 # Questions
-print "Creatings questions . . . "
+print "Seeding questions . . . "
 500.times do |i|
   created_at = Faker::Time.backward(730)
   updated_at = created_at
@@ -24,7 +24,7 @@ puts "Done!"
 question_ids = Question.ids
 
 # Answers
-print "Creatings answers . . . "
+print "Seeding answers . . . "
 10000.times do |i|
   question = Question.find(question_ids.sample)
   created_at = question.created_at + rand(14)
@@ -40,14 +40,15 @@ puts "Done!"
 answer_ids = Answer.ids
 
 # Comments
-puts "Creatings comments . . . "
+puts "Seeding comments . . . "
 commentables = [[Question, question_ids],
                 [Answer,   answer_ids  ]]
 50000.times do |i|
   comment_type     = commentables.sample
   commentable_id   = comment_type[1].sample
   commentable_type = comment_type[0].name
-  created_at       = commentable.created_at + rand(14)
+  commentable_date = comment_type[0].find(commentable_id).created_at
+  created_at       = commentable_date + rand(14)
   updated_at       = created_at
   updated_at      += rand(10) if (created_at + 10) < DateTime.now
   Comment.create(body:             Array.new(3) {
@@ -63,10 +64,10 @@ end
 comment_ids = Comment.ids
 
 # Votes
-puts "Creatings votes . . . "
-voteables = [[Question, 'Question', question_ids],
-             [Answer,   'Answer',   answer_ids  ],
-             [Comment,  'Comment',  comment_ids ]]
+puts "Seeding votes . . . "
+voteables = [[Question, question_ids],
+             [Answer,   answer_ids  ],
+             [Comment,  comment_ids ]]
 vote_values = [1, -1]
 100000.times do |i|
   vote_type     = voteables.sample
@@ -80,7 +81,7 @@ vote_values = [1, -1]
 end
 
 # Tags
-print "Creatings tags . . . "
+print "Seeding tags . . . "
 lorem = Array.new(1000) { Faker::Lorem.word }.uniq
 500.times do |i|
   Tag.create(text: lorem[i])
@@ -90,7 +91,7 @@ tag_ids = Tag.ids
 
 #Add randomized tags to Questions aand select answer for 80% of Questions
 print "Adding 'selected answer' and tags to questions . . . "
-Questions.all.each do |question|
+Question.all.each do |question|
   num_tags = rand(6)
   tag_ids.sample(num_tags).each do |tag_id|
     question.tags << Tag.find(tag_id)

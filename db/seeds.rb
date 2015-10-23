eec4ff4bea12f216322618223629952586ae6c74
Damian C. Rossney <dcrossney@gmail.com>
2015-10-23 11:14:41 -0400

    Generate seeds for users and questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Users
+users = Array.new(50) { Faker::Internet.user_name }.uniq
+25.times do |i|
+    User.create(username: users[i],
+                email:    Faker::Internet.email(users[1])
+                password: users[i])
+end
+
+# Questions
+500.times do |i|
+  created_at = Faker::Time.backward(730)
+  updated_at = created_at
+  updated_at += rand(30) if (created_at + 30) < DateTime.now
+  Question.create(title:      Faker::Hacker.say_something_smart,
+                  body:       Faker::Lorem.paragraph,
+                  created_at: created_at,
+                  updated_at: updated_at)
+end
+
+# Answers
+question_ids = Question.ids
+10000.times do |i|
+  question = Question.find(question_ids.sample)
+  created_at = question.created_at + rand(14)
+  updated_at = created_at
+  updated_at += rand(10) if (created_at + 10) < DateTime.now
+  Answer.create(body:       Array.new(3) {
+                              Faker::Hacker.say_something_smart
+                            }.join(" "),
+                created_at: created_at,
+                updated_at: updated_at)
+end

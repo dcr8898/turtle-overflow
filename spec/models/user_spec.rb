28f1a0760e888393a740ec743965844d884ed5bb
Peter Rosal <peter.rosal@gmail.com>
2015-10-23 15:20:43 -0400

    Add user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 describe User do
 
   context "validations" do
-    it { should validate_presence_of :user }
+    it { should validate_presence_of :username }
     it { should validate_presence_of :password }
+    it { should have_secure_password }
   end
 
   context "associations" do

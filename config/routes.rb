467d7ccf43ec97a1ee24b275e59c0d6dc42a872d
Steven Leiva <leiva.steven@gmail.com>
2015-10-24 15:28:39 -0400

    Add a get '/logout' route that also maps to 'sessions#destroy'. Create the minimal users resource necessary to avoid errors when testing through browser.

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,10 @@ Rails.application.routes.draw do
 
   get    'login',  to: 'sessions#new'
   post   'login',  to: 'sessions#create'
+  get    'logout', to: 'sessions#destroy'
   delete 'logout', to: 'sessions#destroy'
 
   resources :tags, only: [:index, :show]
 
+  resources :users, only: [:show]
 end

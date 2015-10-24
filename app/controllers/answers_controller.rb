ebed002dab631acd2069558023b7d4987dec0ee7
Peter Rosal <peter.rosal@gmail.com>
2015-10-24 19:16:03 -0400

    Update to user current_user helper

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,7 +2,7 @@ class AnswersController < ApplicationController
 
   def create
     @answer = Answer.new(answer_params)
-    @answer.user = User.last #current_user
+    @answer.user = current_user
     @answer.question = Question.find(params[:question_id])
     if @answer.save
       redirect_to question_path(@answer.question, anchor: @answer.id)

44cd72881834f52afd72880ed27b46dbdc66ff92
Steven Leiva <leiva.steven@gmail.com>
2015-10-25 13:11:43 -0400

    Add the destroy action to the Questions controller.
    User can only destroy a question if s/he is the owner of that question.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -62,10 +62,17 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def destroy
+    question = Question.find_by(id: params[:id])
+
+    question.destroy
+    flash[:notice] = "You've successfully deleted #{question.title}"
+    redirect_to root_path
+  end
+
   private
 
   def question_params
     params.require(:question).permit(:title, :body)
   end
-
 end

d9e23e2476acc2e8349009c312b6ac77eea14653
Damian C. Rossney <dcrossney@gmail.com>
2015-10-25 02:51:02 -0400

    Add model tests for Question#add_tags method
    Verifies correct function of Question#add_tags method.

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -12,13 +12,13 @@ describe Question do
   context "associations" do
     it { should belong_to :user}
     it { should belong_to :answer}
-    it { should have_many :answers} 
-    it { should have_and_belong_to_many :tags}  
-    it { should have_many :comments} 
-    it { should have_many :votes}  
+    it { should have_many :answers}
+    it { should have_and_belong_to_many :tags}
+    it { should have_many :comments}
+    it { should have_many :votes}
     it "has many answers" do
-    expect {
-      question.answers << FactoryGirl.build(:answer)
+      expect {
+        question.answers << FactoryGirl.build(:answer)
       }.to change { Question.count }.by(1)
     end
   end
@@ -26,4 +26,23 @@ describe Question do
   it "question should be unanswered by default" do
     expect (question.answer).should be_nil
   end
+
+  context "#add_tags" do
+    it "parses valid string and adds corresponding tag objects" do
+      expect {
+        question.add_tags('test1, test2')
+      }.to change { Tag.count }.by(2)
+    end
+    it "does not process empty string" do
+      expect {
+        question.add_tags('')
+      }.not_to change { Tag.count }
+    end
+    it "does not process nil value" do
+      expect {
+        question.add_tags(nil)
+      }.not_to change { Tag.count }
+    end
+  end
+
 end

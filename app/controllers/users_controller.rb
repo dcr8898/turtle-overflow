01cbd10c88057d0aa8885df6ea994ee153c92e63
Steven Leiva <leiva.steven@gmail.com>
2015-10-24 22:51:15 -0400

    Add logic to show action of Users controller to grab all the data that will be used in the show view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find_by(id: params[:id])
+
+    @user_questions = @user.questions.most_voted
+    @user_answers = @user.answers.most_voted
+    @user_comments = @user.comments.most_voted
   end
 end

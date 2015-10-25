69ace9e4e8e5a3ff4faabbe768324e210e0594b4
Peter Rosal <peter.rosal@gmail.com>
2015-10-25 17:20:49 -0400

    Add methods to check if voted and id original vote for update

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -7,6 +7,7 @@ class Vote < ActiveRecord::Base
   validates :value, numericality: { only_integer: true }
   validates :user_id, uniqueness: { scope: [:voteable_type, :voteable_id], message: "You have already voted." }
 
+
   after_save :update_vote_count
 
   def total_votes
@@ -16,6 +17,14 @@ class Vote < ActiveRecord::Base
   def update_vote_count
     self.voteable.votes_count = self.total_votes
     self.voteable.save
+  end
+
+  def has_voted? 
+    Vote.exists?(voteable_id: self.voteable_id, voteable_type: self.voteable_type, user: self.user)
+  end
+
+  def existing_vote
+    Vote.where(voteable_id: self.voteable_id, voteable_type: self.voteable_type, user: self.user)[0]
   end
 
 end

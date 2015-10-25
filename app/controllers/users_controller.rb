bd467f16250badfca6a0c7ce25eedc63fce7731d
Steven Leiva <leiva.steven@gmail.com>
2015-10-25 11:26:22 -0400

    Add a creat action in the Users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -12,5 +20,11 @@ class UsersController < ApplicationController
     @user_questions = @user.questions.most_voted
     @user_answers = @user.answers.most_voted
     @user_comments = @user.comments.most_voted
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end

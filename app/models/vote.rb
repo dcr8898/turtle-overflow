c799fac8674b9d4e10908ab7e1eee467501e50f1
Peter Rosal <peter.rosal@gmail.com>
2015-10-25 15:28:45 -0400

    Add model method to update vote count after vote save

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,10 +1,21 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
-  belongs_to :voteable, polymorphic: true, counter_cache: true
+  belongs_to :voteable, polymorphic: true
 
   validates :user, presence: true
 
   validates :value, numericality: { only_integer: true }
   validates :user_id, uniqueness: { scope: [:voteable_type, :voteable_id], message: "You have already voted." }
+
+  after_save :update_vote_count
+
+  def total_votes
+    self.voteable.votes.sum(:value)
+  end 
+
+  def update_vote_count
+    self.voteable.votes_count = self.total_votes
+    self.voteable.save
+  end
 
 end

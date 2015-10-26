b710d8de5b2e4a6fbd36b4aa3f74687c07a82b60
Damian C. Rossney <dcrossney@gmail.com>
2015-10-26 03:53:24 -0400

    Refactor comments controller specs to account for comments on answers

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
-require 'pry'
 
 describe CommentsController do
 
-  let(:user) { FactoryGirl.create(:user)}
-  let(:valid_attrs) { FactoryGirl.build(:comment).attributes }
-  let(:invalid_attrs){ FactoryGirl.build(:comment, body:nil ).attributes }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:qc_valid_attrs) { FactoryGirl.build(:comment).attributes }
+  let(:qc_invalid_attrs){ FactoryGirl.build(:comment, body:nil ).attributes }
 
   before(:each) do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -13,33 +12,29 @@ describe CommentsController do
 
   describe '#create' do
 
-    it 'creates a comment from valid parameters' do
-      expect do 
-        if valid_attrs["commentable_type"] == "Question"
-          post :create, { comment: valid_attrs, question_id: valid_attrs["commentable_id"] }
-        else
-          post :create, { comment: valid_attrs, answer_id: valid_attrs["commentable_id"] }
-        end
+    it 'creates a question comment from valid parameters' do
+      expect do
+        post :create, { comment: qc_valid_attrs,
+                        question_id: qc_valid_attrs["commentable_id"] }
       end.to change{Comment.count}.by(1)
     end
-
-    it 'redirects after creating a message' do
-      if valid_attrs["commentable_type"] == "Question"
-        post :create, { comment: valid_attrs, question_id: valid_attrs["commentable_id"] }
-        expect(response).to redirect_to question_path(valid_attrs["commentable_id"], anchor: Comment.last.id)
-      else
-        post :create, { comment: valid_attrs, answer_id: valid_attrs["commentable_id"] }
-        expect(response).to redirect_to question_path(valid_attrs["commentable_id"], anchor: Comment.last.id)
-      end
+    it 'redirects after creating a question comment' do
+        post :create, { comment: qc_valid_attrs,
+                        question_id: qc_valid_attrs["commentable_id"] }
+        expect(response).to redirect_to question_path(qc_valid_attrs["commentable_id"],
+                                                      anchor: Comment.last.id)
     end
-
     it 'does not redirect on invalid parameters' do
-      post :create, {comment: invalid_attrs, question_id: valid_attrs["commentable_id"] }
-      expect(response).to redirect_to question_path(valid_attrs["commentable_id"], anchor: 'new_comment')    
+      post :create, {comment: qc_invalid_attrs,
+                     question_id: qc_valid_attrs["commentable_id"] }
+      expect(response).to redirect_to question_path(qc_valid_attrs["commentable_id"],
+                                                    anchor: 'new_comment')
     end
-
     it 'does not create a message if params invalid' do
-      expect{post :create, {comment: invalid_attrs, question_id: valid_attrs["commentable_id"] }}.not_to change{Comment.count}
+      expect{
+        post :create, {comment: qc_invalid_attrs,
+                       question_id: qc_valid_attrs["commentable_id"] }
+      }.not_to change{Comment.count}
     end
 
   end

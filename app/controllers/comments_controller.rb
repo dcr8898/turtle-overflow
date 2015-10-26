cd67e52836a23f6d3e41cd3ee0cf52970f3befd5
Peter Rosal <peter.rosal@gmail.com>
2015-10-26 07:12:06 -0400

    Add edit and update methods for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,6 +20,25 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @comment = Comment.find_by(id: params[:id])
+    @commentable = @comment.commentable
+  end
+
+  def update
+    @comment = Comment.find_by(id: params[:id])
+    if @comment.update(comment_params)
+      if @comment.commentable_type == "Answer"
+        redirect_to question_path(@comment.commentable.question_id, anchor: @comment.id)
+      else
+        redirect_to question_path(@comment.commentable, anchor: @comment.id)
+      end
+    else
+      flash.now.alert = @comment.errors.full_messages.join(', ')
+      render 'comments/edit'
+    end
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:body)
